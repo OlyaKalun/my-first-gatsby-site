5cf3b121908f0461abc6ad78b04339ff4d9ae9ed
Olya Kalun <olyakalun1998@gmail.com>
2021-08-16 17:13:01 +0300

    Create Pages Programmatically from Data

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import React from "react"
-import Layout from "../components/layout"
-import { graphql } from "gatsby"
-import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../../components/layout"
+import { Link, graphql } from "gatsby"
 
 const BlogPage = ({ data }) => {
   return (
@@ -9,9 +8,10 @@ const BlogPage = ({ data }) => {
       {data.allMdx.nodes.map(node => {
         return (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>{node.body}</MDXRenderer>
           </article>
         )
       })}
@@ -27,8 +27,7 @@ export const query = graphql`
           date(formatString: "MMMM D, YYYY")
           title
         }
-        id
-        body
+        slug
       }
     }
   }
